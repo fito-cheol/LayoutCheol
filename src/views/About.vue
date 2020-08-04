e6f02ec035c8919db734a5013d7c7a550a9fd8a4
Cheol_Kim <cheol.kim@fitogether.com>
2020-08-04 17:44:47 +0900

    Init

---
+++ src/views/About.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="about">
+    <layout
+      :draggable="true"
+      :sortable="true"
+      :items="items"
+      cellWidth=100
+      cellHeight=100
+      
+      >
+      <template slot="cell" scope="props">
+          <button style="width:80px; height:80px">{{props.item}} - {{props.index}} / {{props.sort}} </button>
+      </template>
+    </layout>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Layout from '@/components/GridCustom/Layout.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Layout
+  },
+  data () {
+    return {
+      items: [
+        'a',
+        'b',
+        'c',
+      ]
+    }
+  }
+}
+</script>
+
